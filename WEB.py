ed2070fff320f6d0ba0d40286e6c467c3a1fed62
Uday-sidagana <uday.sidgana@gmail.com>
2024-09-10 22:04:48 +0530

    Url extracted and function to Fetch and Save the scraped data in text form is written

--- WEB.py
+++ WEB.py
@@ -4,5 +4,12 @@ import requests
 
 from bs4 import BeautifulSoup
 
+url ="https://thehoppygoatfarm.com/2018/03/egg-yolk-coffee-yup-thing-awesome/"
 
+def FetchAndSave(url, path):
+    r = requests.get(url)
 
+    with open(path, "w") as f:
+        f.write(r.text)
+
+FetchAndSave(url, "WebScraping/Data/text.html")
