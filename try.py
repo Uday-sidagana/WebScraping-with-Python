1d5c18cf9d32214367fe7a95e5ff43f05c9b8969
Uday-sidagana <uday.sidgana@gmail.com>
2024-09-11 12:45:23 +0530

    comment

--- try.py
+++ try.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 with open("sample.html", "r") as f:
     html_doc = f.read()
-
+# Beautiful soup object
 soup = BeautifulSoup(html_doc, "html.parser")
 
 '''print(soup.title)
